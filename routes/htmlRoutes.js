969a26ec643be129c1b06e4ad8e72e78d51424b0
Caren B <carenpbriones@gmail.com>
2019-10-22 00:24:37 -0700

    Added articles to index page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Article.find({})
       .then(function(dbArticles) {
-        res.render("index", dbArticles);
+        res.render("index", 
+        {articles: dbArticles});
       })
       .catch(function(err) {
         console.log(err);
